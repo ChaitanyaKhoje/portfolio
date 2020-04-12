30637b98027155c7cc0e379406a5ca8d6d222921
ChaitanyaKhoje <chaitanyakhoje9@gmail.com>
2020-04-11 23:45:50 -0700

    new portfolio

---
+++ gatsby-config.js
@@ -0,0 +1,84 @@
+'use strict'
+
+module.exports = {
+  siteMetadata: {
+    title: 'Luke Secomb Digital | Develop & Photograph',
+    description: '.',
+    siteUrl: 'https://lukesecomb.digital',
+    author: {
+      name: 'Luke Secomb',
+      url: 'https://lukesecomb.digital',
+      email: 'hello@lukesecomb.digital',
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Luke Secomb Digital | Develop & Photograph`,
+        short_name: `Luke Secomb Digital`,
+        start_url: `/`,
+        background_color: `#141414`,
+        theme_color: `#FFC87F`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_CODE,
+      },
+    },
+    'gatsby-transformer-json',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+  ],
+}
